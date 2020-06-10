38e84bff980fa2ba7e915d5dfafd52b2fd2c727f
Zhi Keng He <zhikhe@gmail.com>
2020-06-09 22:26:04 -0400

    remove curfew note

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -34,7 +34,6 @@
                         form</a>.</p>
             </div>
         {/if}
-        <p class="has-background-warning">Hours may be adjusted due to NYC curfew week of June 1</p>
     {:else}
         <div class="header-bar">
             <h3 class="title is-5">Essentially Open North Brooklyn</h3>
@@ -59,7 +58,6 @@
                         form</a>.</p>
             </div>
         {/if}
-        <p class="has-background-warning">Hours may be adjusted due to NYC curfew week of June 1</p>
     {/if}
 </header>
 
