01b78399252ec04b02f3cd2444879fa52035d26b
Zhi Keng He <zhikhe@gmail.com>
2020-06-16 11:52:36 -0400

    Fixes to legend position and save opened state in localstorage

--- src/components/map/Legend.svelte
+++ src/components/map/Legend.svelte
@@ -1,7 +1,13 @@
 <script>
     import {categoryGroups, styles} from '../../constants'
 
-    let opened = true
+    let opened = localStorage.getItem('header') === null ? true : JSON.parse(localStorage.getItem('header'));
+
+    function toggleOpen(){
+        //toggle and save to local storage
+        opened = !opened
+        localStorage.setItem('header', opened)
+    }
 
     //group similar colors
     const groups = styles.filter(i => i.group).reduce((groups, i) => {
@@ -24,7 +30,7 @@
 <div class="legend" id="legend">
     <div class="legend-title">
         <h5>Legend</h5>
-        <p><a href="#legend" aria-label="Hide map legend" on:click={() => opened = !opened}>{opened ? 'Hide' : 'Show'}</a></p>
+        <p><a href="#legend" aria-label="Hide map legend" on:click={toggleOpen}>{opened ? 'Hide' : 'Show'}</a></p>
     </div>
     {#if opened}
         {#each Object.values(groups) as group}
@@ -41,31 +47,34 @@
         line-height: 18px;
         color: #212121;
         position: absolute;
-        left: 10px;
-        top: 10px;
-        z-index: 2;
+        right: 5px;
+        bottom: 30px;
+        z-index: 200;
+        border-radius: 3px;
         background-color: rgba(255, 255, 255, 0.8);
-        padding: 5px 10px;
+        box-shadow: 0px 1px 2px rgba(136, 136, 136, 0.6);
+        padding: 5px 5px;
+        margin-bottom: 0px;
         max-width: 80%;
     }
-
     .legend-title {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
     }
-
-    .legend-title p {
-        margin-left: 1rem;
+    .legend-title p{
+        margin-bottom: 0;
     }
-
+    .legend-title h5{
+        margin-right: 1rem;
+        margin-bottom: 0;
+    }
     .legend-item {
         letter-spacing: -0.2px;
         margin-right: 1rem;
         display: flex;
         flex-direction: row;
     }
-
     .legend-item .marker {
         margin-right: 1rem;
         width: 16px;
